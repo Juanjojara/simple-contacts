886b8cd0f8b3915e075b655373537340f80064e6
fc1195 <juanjose.jaralaconich@allitude.it>
2025-11-19 11:58:25 +0100

    add images, gestione eventi click

--- src/app/contact-list/contact-list.ts
+++ src/app/contact-list/contact-list.ts
@@ -9,4 +9,10 @@ import { Contact } from '../contact/contact';
 })
 export class ContactList {
 
+  list = [
+    {id: 0, firstName: 'John', lastName: 'Brown', phone: '21321321', email: 'john@mail.com'},
+    {id: 1, firstName: 'Lucy', lastName: 'Pink', phone: '4324324', email: ''},
+    {id: 2, firstName: 'Emma', lastName: 'Green', phone: '', email: 'alice@mail.com'},
+    {id: 3, firstName: 'Bob', lastName: 'White', phone: '', email: ''},
+  ]
 }
--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, input, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,27 +7,38 @@ import { Component, computed, effect, signal, WritableSignal } from '@angular/co
   styleUrl: './contact.css',
 })
 export class Contact {
-  id: WritableSignal<number> = signal(1);
-  firstName = signal('John');
-  lastName = signal('Brown');
-  phone = signal('+39 334443223');
+  //id: WritableSignal<number> = signal(1);
+  id = input(1);
+  firstName = input('John');
+  lastName = input('Brown');
+  phone = input('+39 334443223');
   //phone = signal(false);
-  //email = signal('john.brown@mail.com');
-  email = signal(null);
+  email = input('john.brown@mail.com');
+  //email = input(null);
   emailLink = computed(() => ("emailto:" + this.email()));
   phoneLink = computed(() => `tel:${this.phone()}`);
   fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+  picture = computed(() => `/images/${this.firstName().toLowerCase()}.svg`);
+
+  showDetails = signal(false);
 
   constructor(){
     //setInterval(() => (this.firstName += '#'), 1000);
     //setInterval(() => (this.firstName.update(val => val +'€')), 1000);
     //setInterval(() => (this.firstName.set('Mark' + Math.random())), 1000);
 
-    setInterval(() => (this.firstName.set('Mark' + Math.random()), this.lastName.set('White' + Math.random())), 3000);
+    //setInterval(() => (this.firstName.set('Mark' + Math.random()), this.lastName.set('White' + Math.random())), 3000);
 
     effect(() => {
       console.log(`Fullname has changed to ${this.fullName()}`);
+      console.log(`ShowDetails has changed to ${this.showDetails()}`);
     });
+
+    
+  }
+
+  switchShowDetails() {
+      this.showDetails.update((val) => val = !val)
   }
 }
 
