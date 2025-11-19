1c77e64b253d8236acb19f7adb0c645fa7cd023e
fc1195 <juanjose.jaralaconich@allitude.it>
2025-11-19 10:39:51 +0100

    Computed attributes e condizionali su html

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -11,12 +11,23 @@ export class Contact {
   firstName = signal('John');
   lastName = signal('Brown');
   phone = signal('+39 334443223');
-  email = signal('john.brown@mail.com');
+  //phone = signal(false);
+  //email = signal('john.brown@mail.com');
+  email = signal(null);
+  emailLink = computed(() => ("emailto:" + this.email()));
+  phoneLink = computed(() => `tel:${this.phone()}`);
+  fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
 
   constructor(){
     //setInterval(() => (this.firstName += '#'), 1000);
     //setInterval(() => (this.firstName.update(val => val +'€')), 1000);
-    setInterval(() => (this.firstName.set('Mark' + Math.random())), 1000);
+    //setInterval(() => (this.firstName.set('Mark' + Math.random())), 1000);
+
+    setInterval(() => (this.firstName.set('Mark' + Math.random()), this.lastName.set('White' + Math.random())), 3000);
+
+    effect(() => {
+      console.log(`Fullname has changed to ${this.fullName()}`);
+    });
   }
 }
 
